817c929ffd09aaf83aa7784581ef57c683c24399
Joshua Soberg <j15soberg@gmail.com>
2023-12-04 21:29:35 -0500

    Add solution for day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -7,6 +7,9 @@ fun main() {
     val input = readInput("Day04-input")
     val part1ScratchcardPoints = Day04Part1.calculateScratchPoints(input)
     println("Part 1 - Scratchcard points: $part1ScratchcardPoints")
+
+    val part2NumberOfScratchcards = Day04Part2.calculateNumScratchCards(input)
+    println("Part 2 - Number of Scratchcards: $part2NumberOfScratchcards")
 }
 
 private object Day04Part1 {
@@ -15,6 +18,44 @@ private object Day04Part1 {
         val scratchcards = ScratchcardParser()
             .parse(input)
         return scratchcards.sumOf(Scratchcard::calculatePoints)
+    }
+}
+
+private object Day04Part2 {
+
+    fun calculateNumScratchCards(input: List<String>): Int {
+        val scratchcards = ScratchcardParser()
+            .parse(input)
+        return ScratchcardCollector(scratchcards)
+            .calculateTotalNumberOfScratchcards()
+    }
+
+    class ScratchcardCollector(
+        private val scratchcards: List<Scratchcard>,
+    ) {
+        private val idToScratchcardMap = scratchcards.associateBy { it.id }
+
+        fun calculateTotalNumberOfScratchcards(): Int {
+            val wonCopyIds = mutableListOf<Int>()
+            scratchcards.forEach { scratchcard ->
+                wonCopyIds.addAll(collectWonCopies(scratchcard))
+            }
+            return scratchcards.size + wonCopyIds.size
+        }
+
+        private fun collectWonCopies(scratchcard: Scratchcard): List<Int> {
+            val wonCopyIds = mutableListOf<Int>()
+            val numWinners = scratchcard.numWinners()
+            if (numWinners > 0) {
+                for (id in (scratchcard.id + 1)..(scratchcard.id + numWinners)) {
+                    wonCopyIds.add(id)
+                    idToScratchcardMap[id]?.let { copiedScratchcard ->
+                        wonCopyIds.addAll(collectWonCopies(copiedScratchcard))
+                    }
+                }
+            }
+            return wonCopyIds
+        }
     }
 }
 
@@ -50,14 +91,19 @@ data class Scratchcard(
     val scratchedNumbers: HashSet<Int>,
 ) {
     fun calculatePoints(): Int {
+        val numWinners = numWinners()
+        return if (numWinners > 0) {
+            2.0.pow((numWinners - 1).toDouble()).toInt()
+        } else 0
+    }
+
+    fun numWinners(): Int {
         var numWinners = 0
         scratchedNumbers.forEach { scratched ->
             if (winningNumbers.contains(scratched)) {
                 numWinners++
             }
         }
-        return if (numWinners > 0) {
-            2.0.pow((numWinners - 1).toDouble()).toInt()
-        } else 0
+        return numWinners
     }
 }
