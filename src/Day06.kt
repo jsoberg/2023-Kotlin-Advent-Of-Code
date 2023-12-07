4f16b363330e3ed71ec8a2b57e59c10fdd1024e8
Joshua Soberg <j15soberg@gmail.com>
2023-12-06 20:54:10 -0500

    Day 6 part 2 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -5,13 +5,16 @@ fun main() {
     val input = readInput("Day06-input")
     val part1WaysToBeatRecord = Day06Part1.calculateNumWaysToBeatRecord(input)
     println("Part 1 - Number of Ways to Beat Record: $part1WaysToBeatRecord")
+
+    val part2WaysToBeatRecord = Day06Part2.calculateNumWaysToBeatRecord(input)
+    println("Part 2 - Number of Ways to Beat Combined Record: $part2WaysToBeatRecord")
 }
 
 private object Day06Part1 {
 
-    fun calculateNumWaysToBeatRecord(input: List<String>): Int {
-        val records = RaceParser().parse(input)
-        var numberOfWaysToBeatResult = 1
+    fun calculateNumWaysToBeatRecord(input: List<String>): Long {
+        val records = RaceParser().parsePart1(input)
+        var numberOfWaysToBeatResult = 1L
         records.forEach { record ->
             val numWaysToBeat = record.calculateNumberOfWaysToBeat()
             if (numWaysToBeat > 0) {
@@ -22,9 +25,17 @@ private object Day06Part1 {
     }
 }
 
+private object Day06Part2 {
+
+    fun calculateNumWaysToBeatRecord(input: List<String>): Long {
+        val record = RaceParser().parsePart2(input)
+        return record.calculateNumberOfWaysToBeat()
+    }
+}
+
 class RaceParser {
 
-    fun parse(input: List<String>): List<RaceRecord> {
+    fun parsePart1(input: List<String>): List<RaceRecord> {
         val raceTimes = input.first { it.startsWith("Time") }
             .parseNumbers()
         val records = input.first { it.startsWith("Distance") }
@@ -40,15 +51,30 @@ class RaceParser {
     private fun String.parseNumbers() =
         split(':')[1]
             .findNumbers()
-            .map { it.toInt() }
+            .map { it.toLong() }
+
+    fun parsePart2(input: List<String>): RaceRecord {
+        val raceTimes = input.first { it.startsWith("Time") }
+            .parseNumbers()
+            .joinToString(separator = "")
+            .toLong()
+        val records = input.first { it.startsWith("Distance") }
+            .parseNumbers()
+            .joinToString(separator = "")
+            .toLong()
+        return RaceRecord(
+            raceTimeMs = raceTimes,
+            raceRecordDistanceMm = records,
+        )
+    }
 }
 
 data class RaceRecord(
-    val raceTimeMs: Int,
-    val raceRecordDistanceMm: Int,
+    val raceTimeMs: Long,
+    val raceRecordDistanceMm: Long,
 ) {
-    fun calculateNumberOfWaysToBeat(): Int {
-        var numWaysToBeat = 0
+    fun calculateNumberOfWaysToBeat(): Long {
+        var numWaysToBeat = 0L
         for (startTime in 1 until raceTimeMs) {
             val timeLeftToRunRace = raceTimeMs - startTime
             val distanceFromStartTime = timeLeftToRunRace * startTime
