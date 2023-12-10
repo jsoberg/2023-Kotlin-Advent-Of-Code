9369e5860439e82ff9322e2aa851f0e817593e5f
Joshua Soberg <j15soberg@gmail.com>
2023-12-10 10:45:02 -0500

    Fix link header

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,4 +1,4 @@
-// https://adventofcode.com/2023/day/6
+// https://adventofcode.com/2023/day/10
 fun main() {
     val input = readInput("Day10-test")
     val part1StepsToFarthestPosition = Day10Part1.calculateStepsToFarthestPosition(input)
